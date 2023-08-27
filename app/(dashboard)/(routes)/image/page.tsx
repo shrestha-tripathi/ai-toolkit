2be31c4a7c10cb6717c1d8a233b68e75f6ed9c13
shresth <shrestha.tripathi@gmail.com>
2023-08-27 05:47:49 +0530

    Chat and Code completed

---
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -0,0 +1,126 @@
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+import Heading from "@/components/heading";
+import { ImageIcon } from "lucide-react";
+import { useForm } from "react-hook-form";
+
+import { formSchema } from "./constants";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Empty } from "@/components/empty";
+import { Loader } from "@/components/loader";
+import { cn } from "@/lib/utils";
+import { UserAvatar } from "@/components/user-avatar";
+import { BotAvatar } from "@/components/bot-avatar";
+
+const ImagePage = () => {
+
+    const router = useRouter();
+
+    const [images, setImages] = useState<string[]>([]);
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            prompt: "",
+            amount: "1",
+            resolution: "512x512"
+        }
+    });
+
+
+    const isLoading = form.formState.isSubmitting;
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            setImages([]);
+            const response = await axios.post('/api/image', values);
+
+            const urls = response.data.map((image: { url: string }) => image.url);
+
+            setImages(urls);
+
+            form.reset();
+        } catch (error: any) {
+            // Open Pro Modal
+            console.log(error);
+        } finally {
+            router.refresh();
+        }
+    }
+
+    return (
+        <div>
+            <Heading
+                title="Image Generation"
+                description="Make AI imagine your thoughts."
+                icon={ImageIcon}
+                iconColor="text-pink-700"
+                bgColor="bg-pink-700/10"
+
+            />
+            <div className="px-4 lg:px-8">
+                <div>
+                    <Form {...form}>
+                        <form
+                            onSubmit={form.handleSubmit(onSubmit)}
+                            className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm
+                            grid grid-col-12 gap-2s"
+                        >
+                            <FormField
+                                name="prompt"
+                                render={({ field }) => (
+                                    <FormItem className="col-span-12 lg:col-span-10">
+                                        <FormControl className="m-0 p-0">
+                                            <Input
+                                                className="border-0 outline-none focus-visible:ring-0
+                                                focus-visible:ring-transparent"
+                                                disabled={isLoading}
+                                                placeholder="A picture of cat playing drums"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                name="amount"
+                                control={form.control} 
+                                render={({ field }) => (
+                                    <FormItem className="col-span-12 lg:col-span-2">
+
+                                    </FormItem>
+                                )}
+                            />
+                            <Button className="col-span-12 lg:col-span-2 w-full" disabled={isLoading}>
+                                Generate
+                            </Button>
+                        </form>
+                    </Form>
+                </div>
+                <div className="space-y-4 mt-4">
+                    {isLoading && (
+                        <div className="p-20">
+                            <Loader />
+                        </div>
+                    )}
+                    {images.length === 0 && !isLoading && (
+                        <Empty label="No Images Generated." />
+                    )}
+                    <div>
+                        Images will render here
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ImagePage;
